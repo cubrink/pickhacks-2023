07529a8f118ce9f2c81094a220448d7749edf4b5
Tanner May <tmay@mst.edu>
2023-04-16 01:54:40 -0500

    feat!: End to end path support

--- safercaver/src/draw_path.py
+++ safercaver/src/draw_path.py
@@ -9,9 +9,15 @@ def multilinspace(values: np.array, n: int):
     return np.append(points, values[-1])
 
 
-def linear_point_cloud(start, end, resolution=30_000):
+def pair_path(start, end, resolution=30_000):
     start_end = np.vstack([start, end])
     pc_arr = np.vstack([multilinspace(start_end[:, i], resolution) for i in range(3)]).T
+
+    return pc_arr
+
+
+def path_point_cloud(path):
+    pc_arr = np.concatenate([pair_path(*pair) for pair in zip(path[:-1], path[1:])])
 
     return pc_from_np(pc_arr)
 
@@ -20,13 +26,9 @@ if __name__ == "__main__":
     point_cloud = load_point_cloud("calisto")
     point_cloud_arr = np.asarray(point_cloud.points)
 
-    starti, endi = np.random.randint(len(point_cloud_arr), size=2)
-    start = point_cloud_arr[starti]
-    end = point_cloud_arr[endi]
-
-    linear_pc = linear_point_cloud(start, end, resolution=10_000)
-
+    path = point_cloud_arr[np.random.randint(len(point_cloud_arr), size=5)]
+    pc = path_point_cloud(path)
     draw_point_clouds(
         (point_cloud, 0, False),
-        (linear_pc, 0, True)
+        (pc, 0, True)
     )
