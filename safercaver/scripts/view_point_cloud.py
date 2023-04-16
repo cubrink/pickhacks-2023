d94489c2467337686af058ed02b8e0b3910ea32f
Curtis Brinker <curtisbrnkr@gmail.com>
2023-04-15 22:51:59 -0500

    View multiple point clouds

--- safercaver/scripts/view_point_cloud.py
+++ safercaver/scripts/view_point_cloud.py
@@ -4,9 +4,11 @@ import open3d as o3d
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) == 1:
         print("Expected usage: view_xyz.py <point_cloud.xyz>")
         exit(1)
 
-    pcd = o3d.io.read_point_cloud(sys.argv[1])
-    o3d.visualization.draw_geometries([pcd])
+    for path in sys.argv[1:]:
+        print(path)
+        pcd = o3d.io.read_point_cloud(path)
+        o3d.visualization.draw_geometries([pcd])
